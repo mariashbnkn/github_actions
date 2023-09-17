8f05fda03e9a5c0b6c4407f4a301b0764b1e51f8
maria <xxmarrria@gmail.com>
2023-09-17 22:54:08 +0700

    change pytest

--- testing/views/test_zodiacs.py
+++ testing/views/test_zodiacs.py
@@ -1,8 +1,10 @@
-from app import create_zodiac
+from app import create_zodiac, delete_zodiacs, delete_zodiac_base
 from models import Zodiacs, db
 
 
 def test_zodiac_details_status_code(client):
+    delete_zodiacs()
+    delete_zodiac_base()
     create_zodiac()
     zodiac = Zodiacs(name="name", date_bth="1999-12-12", id_zodiac=3, zodiac_name="zodiac_name")
     db.session.add(zodiac)
