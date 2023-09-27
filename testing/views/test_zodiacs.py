210bf72828eceda973435bd1cf4155f57dadb830
maria <maria.shubenkina@yandex.ru>
2023-09-27 09:49:13 +0300

    change test_zodiacs.py

--- testing/views/test_zodiacs.py
+++ testing/views/test_zodiacs.py
@@ -3,8 +3,8 @@ from models import Zodiacs, db
 
 
 def test_zodiac_details_status_code(client):
-    delete_zodiacs()
-    delete_zodiac_base()
+    # delete_zodiacs()
+    # delete_zodiac_base()
     create_zodiac()
     zodiac = Zodiacs(name="name", date_bth="1999-12-12", id_zodiac=3, zodiac_name="zodiac_name")
     db.session.add(zodiac)
